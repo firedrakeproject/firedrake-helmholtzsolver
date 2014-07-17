ed866981a617dcb7bc91a5f1c44705caa782cdee
Eike Mueller <eike.h.mueller@gmail.com>
2014-07-17 16:27:34 +0200

    Refactoring of lumped mass classes
    Created a common base class for LumpedMassRT1 and LumpedMassBDFM1 and added a test which compares the kinetic energy for a solid body rotation field as calculated with the full= and lumped mass matrix to the exact value (4/3*pi).

--- source/pressuresolver/lumpedmass.py
+++ source/pressuresolver/lumpedmass.py
@@ -3,7 +3,79 @@ import numpy as np
 from collections import Counter
 from firedrake import *
 
-class LumpedMassRT1(object):
+class LumpedMass(object):
+    ''' Base class for velocity lumped mass matrix.
+
+    :arg V_velocity: Velocity space, has to be a :math:`RT1` space.
+    :arg ignore_lumping: For debugging, this can be set to true to use the
+        full mass matrix in the :class:`multiply()` and :class:`divide()`
+        methods.
+    '''
+    def __init__(self,V_velocity,
+                      ignore_lumping=False):
+        self.V_velocity = V_velocity
+        self.w = TestFunction(self.V_velocity)
+        self.mesh = self.V_velocity.mesh()
+        self.ignore_lumping = ignore_lumping
+        self.dx = self.V_velocity.mesh()._dx
+
+    def test_kinetic_energy(self):
+        '''Check how exactly the kinetic energy can be represented with
+        lumped mass matrix.
+
+        Calculate :math:`E_{kin}= \int \\vec{u}\cdot \\vec{u}\;dx` both with
+        the full- and with the lumped mass matrix and compare
+        '''
+        u_SBR = Function(self.V_velocity)
+        u_SBR.project(Expression(('0','-x[2]','x[1]')))
+        energy_full = assemble(dot(u_SBR,u_SBR)*self.dx)
+        Mu_SBR = Function(self.V_velocity)
+        Mu_SBR.assign(u_SBR)
+        self.multiply(Mu_SBR)
+        energy_lumped = 0.0
+        for (x,y) in zip(u_SBR.dat.data,Mu_SBR.dat.data):
+            energy_lumped += x*y
+        energy_exact = 4.*pi/3.
+        energy_lumped *= 0.5
+        energy_full *= 0.5
+        print 'kinetic energy = '+('%18.12f' % energy_exact)+' [exact]'
+        print '                 '+('%18.12f' % energy_full)+' [full mass matrix]'
+        print '                 '+('%18.12f' % energy_lumped)+' [lumped mass matrix]'
+            
+    def multiply(self,u):
+        '''Multiply by lumped mass matrix
+
+        In-place multiply a velocity field by the lumped mass matrix
+
+        :arg u: velocity field to multiply (will be modified in-place)
+        '''
+        if (self.ignore_lumping):
+            psi = TestFunction(self.V_velocity)
+            w = assemble(dot(self.w,u)*self.dx)
+            u.assign(w)
+        else:
+            self._matmul(self.data,u)
+            
+
+    def divide(self,u):
+        '''Divide by lumped mass matrix
+
+        In-place divide a velocity field by the lumped mass matrix
+
+        :arg u: velocity field to divide (will be modified in-place)
+        '''
+        if (self.ignore_lumping):
+            psi = TestFunction(self.V_velocity)
+            phi = TrialFunction(self.V_velocity)
+            a_mass = assemble(dot(psi,phi)*self.dx)
+            w = Function(self.V_velocity)
+            solve(a_mass, w, u)
+            u.assign(w)
+        else:
+            self._matmul(self.data_inv,u)
+
+
+class LumpedMassRT1(LumpedMass):
     '''Lumped velocity mass matrix.
     
     This class constructs a diagonal lumped velocity mass matrix :math:`M_u^*` 
@@ -33,16 +105,14 @@ class LumpedMassRT1(object):
         where :math:`U^{(i)}` is a solid body rotation field around coordinate
         axis :math:`i` and :math:`V^{(i)} = M_u U^{(i)}`
 
-    :arg V_velocity: Velocity space, currently only :math:`RT1` is supported.
+    :arg V_velocity: Velocity space, has to be a :math:`RT1` space.
     :arg ignore_lumping: For debugging, this can be set to true to use the
         full mass matrix in the :class:`multiply()` and :class:`divide()`
         methods.
     :arg use_SBR: Use mass lumping based on solid body rotation fields.
     '''
     def __init__(self,V_velocity,ignore_lumping=False,use_SBR=True):
-        self.ignore_lumping = ignore_lumping
-        self.V_velocity = V_velocity
-        self.dx = self.V_velocity.mesh()._dx
+        super(LumpedMassRT1,self).__init__(V_velocity,ignore_lumping)
         self.use_SBR = use_SBR
         if (self.use_SBR):
             w = TestFunction(self.V_velocity)
@@ -82,49 +152,21 @@ class LumpedMassRT1(object):
         '''Return :math:`RT1` representation of mass matrix.'''
         return self.data
 
-    def multiply(self,u):
-        '''Multiply velocity field by lumped mass matrix.
+    def _matmul(self,m,u):
+        '''Multiply by diagonal matrix
 
-        Use a direct loop to multiply a function in velocity space by the
-        lumped mass matrix. Note that this is an in-place operation on the
-        input data.
+        In-place multiply a RT1 field by a diagonal matrix, which 
+        is either the lumped mass matrix or its inverse.
 
-        :arg u: Velocity field to be multiplied
+        :arg m: block-diagonal matrix to multiply with
+        :arg u: RT1 field to multiply (will be modified in-place)
         '''
-        if (self.ignore_lumping):
-            psi = TestFunction(self.V_velocity)
-            w = assemble(dot(self.w,u)*self.dx)
-            u.assign(w)
-        else:
-            kernel = '(*u) *= (*data);'
-            par_loop(kernel,direct,
-                     {'u':(u,RW),
-                      'data':(self.data,READ)})
+        kernel = '(*u) *= (*m);'
+        par_loop(kernel,direct,
+                 {'u':(u,RW),
+                  'm':(m,READ)})
 
-    def divide(self,u):
-        '''Divide velocity field by lumped mass matrix.
-
-        Use a direct loop to divide a function in velocity space by the lumped
-        mass matrix. Note that this is an in-place operation on the input data.
-        If the lumping is ignored, the division is implemented by a mass
-        matrix solve.
-
-        :arg u: Velocity field to be divided
-        '''
-        if (self.ignore_lumping):
-            psi = TestFunction(self.V_velocity)
-            phi = TrialFunction(self.V_velocity)
-            a_mass = assemble(dot(psi,phi)*self.dx)
-            w = Function(self.V_velocity)
-            solve(a_mass, w, u)
-            u.assign(w)
-        else:
-            kernel = '(*u) *= (*data_inv);'
-            par_loop(kernel,direct,
-                     {'u':(u,RW),
-                      'data_inv':(self.data_inv,READ)})
-
-class LumpedMassBDFM1(object):
+class LumpedMassBDFM1(LumpedMass):
     '''BDFM1 lumped mass matrix.
 
     Represents a lumped approximation of the BDFM1 mass matrix. The matrix
@@ -138,7 +180,8 @@ class LumpedMassBDFM1(object):
     Dat of suitable shape located on the facets, i.e. the same dof-map as
     for the RT0 space can be used.
 
-    :arg V_velocity: Velocity function space, has to be of type BDFM1
+    :arg V_velocity: Velocity function space, has to be of type
+        :math:`BDFM_1`
     :arg ignore_lumping: For debugging, this can be set to true to use the
         full mass matrix in the :class:`multiply()` and :class:`divide()`
         methods.
@@ -146,15 +189,12 @@ class LumpedMassBDFM1(object):
         some further optimisations. NB: Currently only this option is
         supported in :class:`Jacobi_HigherOrder`
     '''
-    def __init__(self,V_velocity,ignore_mass_lumping=False,
+    def __init__(self,V_velocity,ignore_lumping=False,
                  diagonal_matrix=True):
-        self.V_BDFM1 = V_velocity
-        self.mesh = self.V_BDFM1.mesh()
-        self.dx = self.mesh._dx
+        super(LumpedMassBDFM1,self).__init__(V_velocity,ignore_lumping)
         self.coords = self.mesh.coordinates
         self.n_SBR=4
         self.diagonal_matrix = diagonal_matrix
-        self.ignore_mass_lumping = ignore_mass_lumping
         # Coordinate space
         self.V_coords = self.coords.function_space()
         # Set up map from facets to coordinate dofs
@@ -221,8 +261,8 @@ class LumpedMassBDFM1(object):
         :math:`a_2` are the normal dofs on edge 1 and :math:`a_3` is the
         tangential dof.
         '''
-        cell2dof_map = self.V_BDFM1.cell_node_map()
-        facet2celldof_map = self.V_BDFM1.interior_facet_node_map()
+        cell2dof_map = self.V_velocity.cell_node_map()
+        facet2celldof_map = self.V_velocity.interior_facet_node_map()
         facet2dof_map_val = []
         for (x,idx) in zip(facet2celldof_map.values,
                            self.mesh.interior_facets.local_facet_dat.data):
@@ -252,7 +292,7 @@ class LumpedMassBDFM1(object):
         kernel = op2.Kernel(kernel_code,"set_matrix")
 
         toset = self.V_facets.cell_node_map().toset
-        w = TestFunction(self.V_BDFM1)
+        w = TestFunction(self.V_velocity)
         m_U = Function(self.V_facets,
                        val=op2.Dat(toset**(self.n_SBR,4),
                        dtype=float))
@@ -260,16 +300,16 @@ class LumpedMassBDFM1(object):
                         val=op2.Dat(toset**(self.n_SBR,4),
                         dtype=float))
 
-        U_x = Function(self.V_BDFM1).project(Expression(('0','-x[2]','x[1]')))
-        U_y = Function(self.V_BDFM1).project(Expression(('x[2]','0','-x[0]')))
-        U_z = Function(self.V_BDFM1).project(Expression(('-x[1]','x[0]','0')))
-        U_tilde_x = Function(self.V_BDFM1).project(Expression(('0',
+        U_x = Function(self.V_velocity).project(Expression(('0','-x[2]','x[1]')))
+        U_y = Function(self.V_velocity).project(Expression(('x[2]','0','-x[0]')))
+        U_z = Function(self.V_velocity).project(Expression(('-x[1]','x[0]','0')))
+        U_tilde_x = Function(self.V_velocity).project(Expression(('0',
                                                                '-x[2]*x[0]',
                                                                'x[1]*x[0]')))
-        U_tilde_y = Function(self.V_BDFM1).project(Expression(('x[2]*x[1]',
+        U_tilde_y = Function(self.V_velocity).project(Expression(('x[2]*x[1]',
                                                                '0',
                                                                '-x[0]*x[1]')))
-        U_tilde_z = Function(self.V_BDFM1).project(Expression(('-x[1]*x[2]',
+        U_tilde_z = Function(self.V_velocity).project(Expression(('-x[1]*x[2]',
                                                                'x[0]*x[2]',
                                                                '0')))
         MU_x = assemble(dot(w,U_x)*dx)
@@ -325,12 +365,12 @@ class LumpedMassBDFM1(object):
             self.mat_toset = toset**(4,1)
         else:
             self.mat_toset = toset**(4,4)
-        self.Mu_lumped = Function(self.V_facets,
-                                  val=op2.Dat(self.mat_toset,
-                                  dtype=float))
-        self.Mu_lumped_inv = Function(self.V_facets,
-                                  val=op2.Dat(self.mat_toset,
-                                  dtype=float))
+        self.data = Function(self.V_facets,
+                             val=op2.Dat(self.mat_toset,
+                             dtype=float))
+        self.data_inv = Function(self.V_facets,
+                                 val=op2.Dat(self.mat_toset,
+                                 dtype=float))
         if (self.diagonal_matrix):
             kernel = '''{
               for (int mu=0;mu<4;++mu) {
@@ -342,20 +382,20 @@ class LumpedMassBDFM1(object):
                   b += u_tmp*u_tmp;
                   r += u_tmp*v_tmp;
                 }
-                Mu_lumped[mu] = b/r;
-                Mu_lumped_inv[mu] = r/b;
+                data[mu] = b/r;
+                data_inv[mu] = r/b;
               }
             }'''
             par_loop(kernel,direct,
                      {'u':(m_U,READ),
                       'v':(m_MU,READ),
-                      'Mu_lumped':(self.Mu_lumped,WRITE),
-                      'Mu_lumped_inv':(self.Mu_lumped_inv,WRITE)})
+                      'data':(self.data,WRITE),
+                      'data_inv':(self.data_inv,WRITE)})
         else:
             d_U = m_U.dat.data
             d_MU = m_MU.dat.data
-            d_Mu_lumped = self.Mu_lumped.dat.data
-            d_Mu_lumped_inv = self.Mu_lumped_inv.dat.data
+            d_data = self.data.dat.data
+            d_data_inv = self.data_inv.dat.data
 
             # Build matrix basis for local lumped mass matrix
             # NB: Currently basis is the diagonal basis, so will
@@ -368,7 +408,7 @@ class LumpedMassBDFM1(object):
             n_basis = len(a_basis)
 
             # Loop over all edges and construct the lumped matrix
-            for (U,V,i) in zip(d_U,d_MU,range(len(d_Mu_lumped))):
+            for (U,V,i) in zip(d_U,d_MU,range(len(d_data))):
                 B = np.matrix(np.zeros((n_basis,n_basis),dtype=float))
                 R = np.matrix(np.zeros((n_basis,1),dtype=float))
                 for k in range(self.n_SBR):
@@ -382,10 +422,10 @@ class LumpedMassBDFM1(object):
                         R[mu] += r[0,0]
                 
                 coeff = np.linalg.solve(B,R)
-                d_Mu_lumped[i] = np.zeros((4,4))
+                d_data[i] = np.zeros((4,4))
                 for j in range(n_basis):
-                    d_Mu_lumped[i] += coeff[j,0]*a_basis[j]
-                d_Mu_lumped_inv[i] = np.linalg.inv(d_Mu_lumped[i])
+                    d_data[i] += coeff[j,0]*a_basis[j]
+                d_data_inv[i] = np.linalg.inv(d_data[i])
 
     def _matmul(self,m,u):
         '''Multiply by block-diagonal matrix
@@ -419,37 +459,3 @@ class LumpedMassBDFM1(object):
         op2.par_loop(kernel,self.mesh.interior_facets.set,
                      m.dat(op2.READ,self.facet2dof_map_facets),
                      u.dat(op2.RW,self.facet2dof_map_BDFM1))
-
-    def multiply(self,u):
-        '''Multiply by lumped mass matrix
-
-        In-place multiply a BDFM1 field by the lumped mass matrix
-
-        :arg u: BDFM1 field to multiply (will be modified in-place)
-        '''
-        if (self.ignore_lumping):
-            psi = TestFunction(self.V_velocity)
-            w = assemble(dot(self.w,u)*self.dx)
-            u.assign(w)
-        else:
-            self._matmul(self.Mu_lumped,u)
-            
-
-    def divide(self,u):
-        '''Divide by lumped mass matrix
-
-        In-place divide a BDFM1 field by the lumped mass matrix
-
-        :arg u: BDFM1 field to divide (will be modified in-place)
-        '''
-        if (self.ignore_mass_lumping):
-            psi = TestFunction(self.V_BDFM1)
-            phi = TrialFunction(self.V_BDFM1)
-            a_mass = assemble(dot(psi,phi)*self.dx)
-            w = Function(self.V_BDFM1)
-            solve(a_mass, w, u)
-            u.assign(w)
-        else:
-            self._matmul(self.Mu_lumped_inv,u)
- 
-
