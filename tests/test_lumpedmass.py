edbc8c1da864003889948f714177283c814ac1ef
Eike Mueller <eike.h.mueller@gmail.com>
2014-12-05 11:31:02 +0000

    United common fixtures in one file
    The mesh and function space fixtures are used by all tests, so to avoid duplication I moved them to a separate file.

--- tests/test_lumpedmass.py
+++ tests/test_lumpedmass.py
@@ -2,62 +2,7 @@ from firedrake import *
 from pressuresolver.lumpedmass_diagonal import *
 import numpy as np
 import pytest
-
-
-@pytest.fixture
-def mesh():
-    '''Create 1+1 dimensional mesh by extruding a circle.'''
-    D = 0.1
-    nlayers=4
-    ncells=3
-
-    host_mesh = CircleManifoldMesh(ncells)
-    mesh = ExtrudedMesh(host_mesh,
-                        layers=nlayers,
-                        extrusion_type='radial',
-                        layer_height=D/nlayers)
-
-    return mesh
-
-@pytest.fixture
-def finite_elements():
-    '''Create finite elements of horizontal and vertical function spaces.
-
-    :math:`U_1` = horizontal H1 space
-    :math:`U_2` = horizontal L2 space
-    :math:`V_0` = vertical H1 space
-    :math:`V_1` = vertical L2 space
-    '''
-    # Finite elements
-
-    # Horizontal elements
-    U1 = FiniteElement('CG',interval,2)
-    U2 = FiniteElement('DG',interval,1)
-
-    # Vertical elements
-    V0 = FiniteElement('CG',interval,2)
-    V1 = FiniteElement('DG',interval,1)
-
-    return U1, U2, V0, V1
-
-@pytest.fixture
-def W2_horiz(finite_elements,mesh):
-    '''HDiv space for horizontal velocity component.
-            
-    Build vertical horizontal space :math:`W_2^{h} = Hdiv(U_1\otimes V_1)`
-    
-    :arg finite_elements: Horizontal and vertical finite element
-    :arg mesh: Underlying extruded mesh
-    '''
-
-    U1, U2, V0, V1 = finite_elements
-
-    # Three dimensional elements
-    W2_elt = HDiv(OuterProductElement(U1,V1))
-
-    W2_horiz = FunctionSpace(mesh,W2_elt)
-    
-    return W2_horiz
+from fixtures import *
     
 def test_diagonal(W2_horiz):
     '''Check that we really picked out the diagonal elements of the mass matrix.
