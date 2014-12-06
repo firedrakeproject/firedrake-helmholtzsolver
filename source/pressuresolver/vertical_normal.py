9284dcf2d2ef023caef80ca3e130ebb0757055bc
Eike Mueller <eike.h.mueller@gmail.com>
2014-12-06 18:36:51 +0000

    Added class for representing unit vertical normal vector field
    This class can be used to build a DG0^3 vector function space which represents the unit vertical normal in each cell. Also added tests which check the resulting field for orthogonality, normalisation and orientation.

---
+++ source/pressuresolver/vertical_normal.py
@@ -0,0 +1,111 @@
+from firedrake import *
+import sys, petsc4py
+import numpy as np
+
+petsc4py.init(sys.argv)
+
+from petsc4py import PETSc
+
+
+class VerticalNormal(object):
+    def __init__(self,mesh):
+        '''Class for constructing a vertical normal field on a given extruded mesh`.
+
+        :arg mesh: extruded mesh
+        '''
+        self._mesh = mesh
+        self._build_zhat()
+
+    @property
+    def zhat(self):
+        return self._zhat
+
+    def _build_zhat(self):
+        '''Calculate vector field representing the unit normal in each cell.
+        '''
+        normal_fs = VectorFunctionSpace(self._mesh,'DG',0)
+        self._zhat = Function(normal_fs)
+        host_mesh = self._mesh._old_mesh
+        # Topological dimension of the underlying grid, can be 1 or 2
+        host_dimension = host_mesh._ufl_cell.topological_dimension()
+        if (host_dimension == 1):
+            kernel_code = '''build_normal(double **base_coords,
+                                          double **normals) {
+              const int ndim=2;
+              const int nvert=2;
+              double dx[ndim];
+              double xavg[ndim];
+              double n[ndim];
+              // Calculate vector between the two points
+              for (int i=0;i<ndim;++i) { // Loop over dimensions
+                dx[i] = base_coords[1][i] - base_coords[0][i];
+              }
+              // Rotate by 90 degrees to get normal
+              n[0] = -dx[1];
+              n[1] = +dx[0];
+              // Calculate vector at centre of edge
+              for (int i=0;i<ndim;++i) { // Loop over dimensions
+                xavg[i] = 0.0;
+                for (int j=0;j<nvert;++j) { // Loop over vertices
+                  xavg[i] += base_coords[j][i];
+                }
+              }
+              // Calculate ||n|| and n.x_avg
+              double nrm = 0.0;
+              double n_dot_xavg = 0.0;
+              for (int i=0;i<ndim;++i) {
+                nrm += n[i]*n[i];
+                n_dot_xavg += n[i]*xavg[i];
+              }
+              nrm = sqrt(nrm);
+              // Orient correctly
+              nrm *= (n_dot_xavg<0?-1:+1);
+              for (int i=0;i<ndim;++i) {
+                normals[0][i] = n[i]/nrm;
+              }
+            }'''
+        else:
+            kernel_code = '''build_normal(double **base_coords,
+                                          double **normals) {
+              const int ndim=3;
+              const int nvert=3;
+              double dx[2][ndim];
+              double xavg[ndim];
+              double n[ndim];
+              // Calculate vector between the two points
+              for (int i=0;i<ndim;++i) { // Loop over dimensions
+                for (int j=0;j<2;++j) {
+                  dx[j][i] = base_coords[1+j][i] - base_coords[0][i];
+                }
+              }
+              // Calculate normal
+              for (int i=0;i<ndim;++i) {
+                n[i] = dx[0][(1+i)%3]*dx[1][(2+i)%3] 
+                     - dx[0][(2+i)%3]*dx[1][(1+i)%3];
+              }
+              // Calculate vector at centre of edge
+              for (int i=0;i<ndim;++i) { // Loop over dimensions
+                xavg[i] = 0.0;
+                for (int j=0;j<nvert;++j) { // Loop over vertices
+                  xavg[i] += base_coords[j][i];
+                }
+              }
+              // Calculate ||n|| and n.x_avg
+              double nrm = 0.0;
+              double n_dot_xavg = 0.0;
+              for (int i=0;i<ndim;++i) {
+                nrm += n[i]*n[i];
+                n_dot_xavg += n[i]*xavg[i];
+              }
+              nrm = sqrt(nrm);
+              // Orient correctly
+              nrm *= (n_dot_xavg<0?-1:+1);
+              for (int i=0;i<ndim;++i) {
+                normals[0][i] = n[i]/nrm;
+              }
+            }'''
+        kernel = op2.Kernel(kernel_code,'build_normal')
+        base_coords = host_mesh.coordinates
+        op2.par_loop(kernel,self._zhat.cell_set,
+                     base_coords.dat(op2.READ,base_coords.cell_node_map()),
+                     self._zhat.dat(op2.WRITE,self._zhat.cell_node_map()))
---
+++ tests/test_vertical_normal.py
@@ -0,0 +1,183 @@
+from firedrake import *
+from pressuresolver.vertical_normal import *
+import numpy as np
+import pytest
+
+@pytest.fixture
+def mesh_2d():
+    '''Create 1+1 dimensional mesh by extruding a circle.'''
+    D = 1.0
+    nlayers=4
+    ncells=16
+
+    host_mesh = CircleManifoldMesh(ncells)
+    mesh_2d = ExtrudedMesh(host_mesh,
+                           layers=nlayers,
+                           extrusion_type='radial',
+                           layer_height=D/nlayers)
+
+    return mesh_2d
+
+@pytest.fixture
+def mesh_3d():
+    '''Create 2+1 dimensional mesh by extruding an icosahedral mesh.'''
+    D = 1.0
+    nlayers=4
+    refinement_level=1
+
+    host_mesh = UnitIcosahedralSphereMesh(refinement_level)
+    mesh_3d = ExtrudedMesh(host_mesh,
+                           layers=nlayers,
+                           extrusion_type='radial',
+                           layer_height=D/nlayers)
+
+    return mesh_3d
+
+def test_zhat_2d_plot(mesh_2d):
+    '''Create a vertical normal field on a 1+1 dimensional extruded grid and
+    save to disk.
+
+    :arg mesh_2d: 1+1 dimensional mesh 
+    '''
+    vertical_normal = VerticalNormal(mesh_2d)
+    zhat = vertical_normal.zhat
+    DFile = File('zhat_2d.pvd')
+    DFile << zhat 
+    assert True
+
+def test_zhat_2d_length(mesh_2d):
+    '''Create a vertical normal field on a 1+1 dimensional extruded grid and
+    check that the vectors have the correct length.
+
+    :arg mesh_2d: 1+1 dimensional mesh 
+    '''
+    vertical_normal = VerticalNormal(mesh_2d)
+    zhat = vertical_normal.zhat
+    nrm = [x**2+y**2 for x,y in zhat.dat.data]
+    assert np.allclose(nrm, 1.0)
+
+def test_zhat_2d_orthogonality_and_orientation(mesh_2d):
+    '''Create a vertical normal field on a 1+1 dimensional extruded grid and
+    check that the vectors are orthogonal to the bottom facet.
+
+    :arg mesh_2d: 1+1 dimensional mesh 
+    '''
+    vertical_normal = VerticalNormal(mesh_2d)
+    zhat = vertical_normal.zhat
+    host_mesh = mesh_2d._old_mesh
+    base_coords = host_mesh.coordinates
+    fs_ortho = FunctionSpace(mesh_2d,'DG',0)
+    is_orthogonal = Function(fs_ortho,dtype=int)
+    kernel_code = '''void check_orthogonality(double **base_coords,
+                                              double **normals,
+                                              int **is_orthogonal) {
+                const int ndim=2;
+                const int nvert=2;
+                double dx[ndim];
+                double xavg[ndim];
+                // Calculate vector dx between the two points
+                // and facet centre vector xavg
+                for (int i=0;i<ndim;++i) { // Loop over dimensions
+                  dx[i] = base_coords[1][i] - base_coords[0][i];
+                  xavg[i] = 0.0;
+                  for (int j=0;j<nvert;++j) { // Loop over vertices
+                    xavg[i] += base_coords[j][i];
+                  }
+                }
+                double n_dot_xavg = 0.0;
+                double n_dot_dx = 0.0;
+                for (int i=0;i<ndim;++i) {
+                  n_dot_xavg += normals[0][i]*xavg[i];
+                  n_dot_dx += normals[0][i]*dx[i];
+                }
+                is_orthogonal[0][0] = (n_dot_xavg > 0.0) && (abs(n_dot_dx) < 1.E-9);
+              }'''
+    kernel = op2.Kernel(kernel_code,'check_orthogonality')
+    op2.par_loop(kernel,zhat.cell_set,
+                 base_coords.dat(op2.READ,base_coords.cell_node_map()),
+                 zhat.dat(op2.READ,zhat.cell_node_map()),
+                 is_orthogonal.dat(op2.WRITE,is_orthogonal.cell_node_map()))
+    assert np.allclose(is_orthogonal.dat.data, 1.0)
+
+
+def test_zhat_3d_plot(mesh_3d):
+    '''Create a vertical normal field on a 2+1 dimensional extruded grid and
+    save to disk.
+
+    :arg mesh_3d: 2+1 dimensional mesh 
+    '''
+    vertical_normal = VerticalNormal(mesh_3d)
+    zhat = vertical_normal.zhat
+    DFile = File('zhat_3d.pvd')
+    DFile << zhat 
+    assert True
+
+def test_zhat_3d_length(mesh_3d):
+    '''Create a vertical normal field on a 2+1 dimensional extruded grid and
+    check that the vectors have the correct length.
+
+    :arg mesh_3d: 2+1 dimensional mesh 
+    '''
+    vertical_normal = VerticalNormal(mesh_3d)
+    zhat = vertical_normal.zhat
+    nrm = [x**2+y**2+z**2 for x,y,z in zhat.dat.data]
+    assert np.allclose(nrm, 1.0)
+
+def test_zhat_3d_orthogonality_and_orientation(mesh_3d):
+    '''Create a vertical normal field on a 2+1 dimensional extruded grid and
+    check that the vectors are orthogonal to the bottom facet.
+
+    :arg mesh_2d: 2+1 dimensional mesh 
+    '''
+    vertical_normal = VerticalNormal(mesh_3d)
+    zhat = vertical_normal.zhat
+    host_mesh = mesh_3d._old_mesh
+    base_coords = host_mesh.coordinates
+    fs_ortho = FunctionSpace(mesh_3d,'DG',0)
+    is_orthogonal = Function(fs_ortho,dtype=int)
+    kernel_code = '''void check_orthogonality(double **base_coords,
+                                              double **normals,
+                                              int **is_orthogonal) {
+                const int ndim=3;
+                const int nvert=3;
+                double dx[2][ndim];
+                double xavg[ndim];
+                // Calculate vector dx between the two points
+                // and facet centre vector xavg
+                for (int i=0;i<ndim;++i) { // Loop over dimensions
+                  for (int j=0;j<2;++j) {
+                    dx[j][i] = base_coords[1+j][i] - base_coords[0][i];
+                  }
+                  xavg[i] = 0.0;
+                  for (int j=0;j<nvert;++j) { // Loop over vertices
+                    xavg[i] += base_coords[j][i];
+                  }
+                }
+                double n_dot_xavg = 0.0;
+                double n_dot_dx[2];
+                for (int j=0;j<2;++j) {
+                  n_dot_dx[j] = 0.0;
+                }
+                for (int i=0;i<ndim;++i) {
+                  n_dot_xavg += normals[0][i]*xavg[i];
+                  for (int j=0;j<2;++j) {
+                    n_dot_dx[j] += normals[0][i]*dx[j][i];
+                  }
+                }
+                is_orthogonal[0][0] = (n_dot_xavg > 0.0) 
+                                   && (abs(n_dot_dx[0]) < 1.E-9)
+                                   && (abs(n_dot_dx[1]) < 1.E-9);
+              }'''
+    kernel = op2.Kernel(kernel_code,'check_orthogonality')
+    op2.par_loop(kernel,zhat.cell_set,
+                 base_coords.dat(op2.READ,base_coords.cell_node_map()),
+                 zhat.dat(op2.READ,zhat.cell_node_map()),
+                 is_orthogonal.dat(op2.WRITE,is_orthogonal.cell_node_map()))
+    assert np.allclose(is_orthogonal.dat.data, 1.0)
+
+##############################################################
+# M A I N
+##############################################################
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
