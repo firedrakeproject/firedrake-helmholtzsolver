69fd6cede03a7745b09e542a4e31c1225d59d95a
Eike Mueller <eike.h.mueller@gmail.com>
2015-11-20 18:06:00 +0000

    Add performance logging for application of operator Hhat_h
    The time for the PETSc SpMV for the application of the operator Hhat_h is measured and added to the static performance dictionary of the ParLoop class.

--- source/pressuresolver/operators.py
+++ source/pressuresolver/operators.py
@@ -1,9 +1,13 @@
+import time
+from collections import namedtuple
 from firedrake import *
 from firedrake.petsc import PETSc
 from lumpedmass import *
 import xml.etree.cElementTree as ET
 from firedrake.ffc_interface import compile_form
 from pyop2.profiling import timed_function, timed_region
+from pyop2.base import ParLoop
+from pyop2.performancedata import PerformanceData
 from mpi4py import MPI
 from bandedmatrix import *
 
@@ -240,9 +244,33 @@ class Operator_Hhat(object):
             self._phi_tmp.assign(phi)
             with timed_region('apply_Hhat_h_level_'+str(self._level)):
                 if (self._preassemble_horizontal):
+                    t_start = time.time()
                     with self._BT_B_h_phi.dat.vec as v:
                         with phi.dat.vec_ro as x:
                             self._mat_Hhat_h.mult(x,v)
+                    t_end = time.time()
+
+                    label = 'petsc ax[apply_Hhat_h_level_'+str(self._level)+']'
+                    DataVolume = namedtuple("DataVolume", ["loads", "stores"])
+                    with phi.dat.vec_ro as v:
+                        m_row = v.getSize()
+                    n_nz = self._mat_Hhat_h.getInfo()['nz_used']
+                    flops = 2*n_nz
+                    loads = 12*(m_row + n_nz)
+                    stores = 8*m_row
+                    perfect_cache_data_volume=DataVolume(loads=loads,
+                                                         stores=stores)
+                    pessimal_cache_data_volume=DataVolume(loads=1,
+                                                          stores=1)
+                    try:
+                        perf = ParLoop.perfdata[label]
+                    except KeyError:
+                        perf = PerformanceData(label,
+                                               flops,
+                                               perfect_cache_data_volume,
+                                               pessimal_cache_data_volume)
+                    perf.add_timing(t_end - t_start)
+                    ParLoop.perfdata[label] = perf
                 else:
                     # Calculate action of B_h
                     assemble(self._B_h_phi_form, tensor=self._B_h_phi)
